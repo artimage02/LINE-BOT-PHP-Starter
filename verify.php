7d04f2456f7810fbe3f24e37f76d04610760106f
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-16 20:29:53 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token= '993c49868f382dbd31dfd38d9068d045
-';
+$access_token= '993c49868f382dbd31dfd38d9068d045';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -8,7 +7,7 @@ $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEARDER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
