371366af34675fe3c1b7d43f441303efdbe62e80
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-16 20:13:32 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
 <?php
-$access_token = 'q9q06pYl87SBup6azoZWERsIrwrMrJEuLTRAdyKQ112aux21/RYTX+HujYqypK8Z6FgFdcF8OBTNl4NaO2qJFlxoNX17z8dx7TW0XnLXdFo1/dhBC8NS3HVg9snydgAtvM6eijz7Sbe28oZgT1ZFEQdB04t89/1O/w1cDnyilFU=';
+q9q06pYl87SBup6azoZWERsIrwrMrJEuLTRAdyKQ112aux21/RYTX+HujYqypK8Z6FgFdcF8OBTNl4NaO2qJFlxoNX17z8dx7TW0XnLXdFo1/dhBC8NS3HVg9snydgAtvM6eijz7Sbe28oZgT1ZFEQdB04t89/1O/w1cDnyilFU= = 'xxxxxxxx';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . q9q06pYl87SBup6azoZWERsIrwrMrJEuLTRAdyKQ112aux21/RYTX+HujYqypK8Z6FgFdcF8OBTNl4NaO2qJFlxoNX17z8dx7TW0XnLXdFo1/dhBC8NS3HVg9snydgAtvM6eijz7Sbe28oZgT1ZFEQdB04t89/1O/w1cDnyilFU=);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
