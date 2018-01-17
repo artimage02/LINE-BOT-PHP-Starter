9f83050786e2e7bac3f34a3d28f9f75ac306606d
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-17 21:42:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = 'มีอะไรให้ช่วยไหม';
+			$text = "มีอะไรให้ช่วยไหม";
 			if $event['message']['text']=='สวัสดี;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
