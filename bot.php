81bb4426f04b29bf9ec373868b2b053f4185c044
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-17 21:40:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = 'มีอะไรให้ช่วยไหม';
 			if $event['message']['text']=='สวัสดี;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "มีอะไรให้ช่วยไหม"
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
