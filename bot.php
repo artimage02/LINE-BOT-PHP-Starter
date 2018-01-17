d6396571f808982cca5f3cfa7d8841093aaf723d
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-17 20:39:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,10 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+
 if($events['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $events['events'][0]['replyToken'];
