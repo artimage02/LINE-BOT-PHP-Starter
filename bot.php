fde518240b8a1f73920fa5f290fd8740f58c450a
artimage02 <35488623+artimage02@users.noreply.github.com>
2018-01-17 14:33:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				     {
             "type":"text",
-            "text":"Hello, user"
+            "text":"Hello"
         },
         {
             "type":"text",
